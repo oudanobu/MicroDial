c3ac48e8aed315dd7eebec6e887c89c7b8357526
oudanobu <n16234637@gmail.com>
2026-06-12 20:18:56 +0800

    refactor: remove unused JString import and mutability

--- rust_core/src/lib.rs
+++ rust_core/src/lib.rs
@@ -1,10 +1,10 @@
 use jni::JNIEnv;
-use jni::objects::{JClass, JString};
+use jni::objects::JClass;
 use jni::sys::jstring;
 
 #[no_mangle]
 pub extern "system" fn Java_com_oudanobu_chronoxide_MainActivity_stringFromJNI(
-    mut env: JNIEnv,
+    env: JNIEnv,
     _class: JClass,
 ) -> jstring {
     let output = env.new_string("ChronOxide Engine: 15MB RAM Mode").expect("Couldn't create java string!");
